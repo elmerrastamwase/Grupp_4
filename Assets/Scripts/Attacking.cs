417d4eb4a57de3af89c84b06ddd8360e2a3f89f9
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2019-02-05 10:49:34 +0100

    WIP on weapon swinging, be warned, very difficult

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -6,8 +6,6 @@ public class Attacking : MonoBehaviour
 {
     void Start()
     {
-        rBody = GetComponent<Rigidbody2D>();
-
         trans = GetComponent<Transform>();
     }
 
@@ -18,7 +16,6 @@ public class Attacking : MonoBehaviour
         AttackScript();
     }
 
-    public Rigidbody2D rBody;
     public int attackSpeed;
     public Transform trans;
 
@@ -26,14 +23,22 @@ public class Attacking : MonoBehaviour
     public int attackDamage;
 
 
+    public int SwingSpeed = 5;
+
     public void AttackScript()
     {
+
         if (attackCooldown >= 3)
         {
+            Vector2 from = new Vector2(5.5f, 1.5f);
+            Vector2 to = new Vector2(-4.5f, -2.5f);
+
+            float angle = Vector2.Angle(from, to);
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
             if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Mouse0))
             {
-                trans.position = new Vector2(1,10);
-                attackCooldown = 0f;
+                trans.localRotation = Quaternion.Lerp(transform.rotation, rotation, SwingSpeed * Time.deltaTime);
             }
             if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Mouse0))
             {
