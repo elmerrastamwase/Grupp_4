c8a261dbe44f677203eb948aa777cbcbba1b2576
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-02-05 08:35:40 +0100

    init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int moveSpeed = 5;
+    public float jumpForce = 6;
+    public Transform feetPos;
+    public float checkRadius = 0.5f;
+    public LayerMask whatIsGround;
+    public float jumpTime = 0.6f;
+    public bool isJumping;
+
+    private bool isGrounded;
+    private Rigidbody2D rbody;
+    private float jumpTimeTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rbody.velocity.y);
+        jumpScript();
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+    }
+
+    public void jumpScript()
+    {
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeTimer = jumpTime;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(rbody.velocity.x, jumpForce);
+        }
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeTimer > 0)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(rbody.velocity.x, jumpForce);
+                jumpTimeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+        
+    }
+}
