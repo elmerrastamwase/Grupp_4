8ba9b9c4ab5547eb4e8a763229faa3e3450de106
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2019-02-05 10:49:01 +0100

    Påbörjat checkpoints

---
+++ Assets/Scripts/Gamemaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemaster : MonoBehaviour
+{
+    private static Gamemaster Level;
+
+    void Awake()
+    {
+        if (Level == null)
+        {
+            Level = this;
+            DontDestroyOnLoad(Level);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
