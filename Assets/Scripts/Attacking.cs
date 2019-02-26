6b4798f200a39cb5828b6b14b61a536dc933000c
felix.emblad <felix.emblad@elev.ga.lbs.se>
2019-02-26 08:52:55 +0100

    reversed the commit

---
+++ Assets/Scripts/Attacking.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacking : MonoBehaviour
+{
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        attackCooldown += Time.deltaTime;
+
+        AttackScript();
+
+        WeaponToPlayer();
+    }
+
+    private Transform trans;
+
+    public float attackCooldown;
+    public int attackDamage;
+
+    public GameObject player;
+
+
+
+    public void AttackScript()
+    {
+        if (attackCooldown >= 0.5f)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                trans.rotation = Quaternion.Euler(0, 0, 0);
+
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    trans.eulerAngles = new Vector3(0, 0, 0);
+                    attackCooldown = 0;
+                }
+            }
+
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                trans.rotation = Quaternion.Euler(0, 180, 0);
+
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    attackCooldown = 0;
+                }
+            }
+
+
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Mouse0))
+            {
+                attackCooldown = 0;
+                trans.eulerAngles = new Vector3(0, 0, 90);
+            }
+
+            GameObject playerScripts = GameObject.FindWithTag("Player");
+            bool isGrounded = playerScripts.GetComponentInParent<PlayerMovement>().isGrounded;
+            
+            if (isGrounded == false)
+            {
+                if (Input.GetKey(KeyCode.S))
+                {
+
+                    if (Input.GetKey(KeyCode.Mouse0))
+                    {
+                        trans.eulerAngles = new Vector3(0, 0, -90);
+                        attackCooldown = 0;
+                    }
+
+                }
+            }
+
+        }
+    }
+
+    public void WeaponToPlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            trans.position = player.transform.position;
+        }
+        else
+        {
+            trans.position = player.transform.position;
+        }
+    }
+}
+
