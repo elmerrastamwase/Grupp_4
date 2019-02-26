1f6f461d60246d5e1c58a1f9f965937bd5042039
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2019-02-26 09:10:30 +0100

    added back stuff

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Gamemaster gm;
+
+    public void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<Gamemaster>();
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (other.CompareTag("Player"))
+            {
+                gm.lastCheckPointPos = transform.position;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gamemaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemaster : MonoBehaviour
+{
+    private static Gamemaster instance;
+    public Vector2 lastCheckPointPos;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
