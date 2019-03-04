5eec929b7519f374143be2731e227dba1025ea88
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-03-04 10:43:40 +0100

    fixed attacking to be more like hollowknight and various buggs

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -6,57 +6,71 @@ public class Attacking : MonoBehaviour
 {
     void Start()
     {
-        trans = GetComponent<Transform>();
-
-        player = GameObject.FindWithTag("Player");
+        GetComponent<BoxCollider2D>().enabled = false;
     }
 
     void Update()
     {
-        attackCooldown += Time.deltaTime;
+        if (attackCooldown > 0)
+        {
+            attackCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            attackCooldown = 0;
+        }
 
         AttackScript();
-
-        WeaponToPlayer();
     }
 
     public Transform trans;
 
     public float attackCooldown;
     public int attackDamage;
-
+    public float attackState;
     public GameObject player;
     public Vector3 Offset = new Vector3(0.5f, 0, 0);
+    public Animator anim;
 
     public void AttackScript()
-    {
-        if (attackCooldown >= 0.5f)
+    {       
+        if (attackCooldown <= 0 && Input.GetKeyDown(KeyCode.K))
         {
-            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.Mouse0))
-            {
-                trans.eulerAngles = new Vector3(0, 0, 0);
-                attackCooldown = 0;
-            }
-            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.Mouse0))
-            {
-                trans.eulerAngles = new Vector3(0, 0, -180);
-                attackCooldown = 0;
-            }
-            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Mouse0))
-            {
-                trans.eulerAngles = new Vector3(0, 0, 90);
-                attackCooldown = 0;
-            }
-            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.Mouse0))
-            {
-                trans.eulerAngles = new Vector3(0, 0, -90);
-                attackCooldown = 0;
-            }
+            GetComponent<BoxCollider2D>().enabled = true;
+            attackState = 0.2f;
         }
-    }
+        if (attackState > 0)
+        {
+            attackState -= Time.deltaTime;
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            attackState = 0;
+            anim.SetBool("isAttacking",false);
+        }
 
-    public void WeaponToPlayer()
-    {
-        trans.position = player.transform.position + Offset;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(-1.5f, 1.5f, 0);
+            anim.SetBool("facingUp", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            transform.Translate(1.5f, -1.5f, 0);
+            anim.SetBool("facingUp", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(-1.5f, -1.5f, 0);
+            anim.SetBool("facingDown", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            transform.Translate(1.5f, 1.5f, 0);
+            anim.SetBool("facingUp", false);
+        }
     }
 }
