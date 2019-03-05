d066bc3a98371edfe3848f5679fee245d58fd609
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2019-03-05 08:54:58 +0100

    working on HP 2. If you press N on my attack scene the display should show the HP you have.

---
+++ Assets/Scripts/devDisplayealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class devDisplayealth : MonoBehaviour
+{
+
+    public TextMeshProUGUI healthDisplayed;
+
+    void Start()
+    {
+        healthDisplayed = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        healthDisplayed.text = PlayerHP.playerHp.ToString();
+
+        turnOnOffDisplay();
+    }
+
+    public void turnOnOffDisplay()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (healthDisplayed.enabled == true)
+            {
+                healthDisplayed.enabled = false;
+            }
+            else if (healthDisplayed.enabled == false)
+            {
+                healthDisplayed.enabled = true;
+            }
+        }
+    }
+}
