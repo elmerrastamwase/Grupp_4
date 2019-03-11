5649315770d11a3f9a533e00ef55aa8cf95596c2
felix.emblad <felix.emblad@elev.ga.lbs.se>
2019-03-11 08:13:17 +0100

    fixing main menu

---
+++ Assets/timedkiller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timedkiller : MonoBehaviour
+{
+    public string sceneToLoad = ("Felix.sandbox");
+    public float startkilltimer =1;
+    public float killtimer =1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (killtimer <= 0)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+       if (killtimer >= 0)
+        {
+            killtimer -= Time.deltaTime;
+        }  
+    }
+    
+         
+    
+
+
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+          killtimer = startkilltimer;
+    }
+    
+        
+      
+    
+    
+    
+                  
+        
+  
+   
+    
+    
+
+}
