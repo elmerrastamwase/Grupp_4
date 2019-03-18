a6a0fe21e204424c72a239f22b388b382dfd5084
Wissam Hattab <wissam.hattab@elev.ga.lbs.se>
2019-03-18 10:18:20 +0100

    Fixed animation error. Started work on parallax effect.

---
+++ Assets/Scripts/parallaxEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxEffect : MonoBehaviour
+{
+    public int mainParallaxSpeed = 5;
+    public int secondaryParallaxSpeed = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
