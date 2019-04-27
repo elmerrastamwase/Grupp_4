2fa901f7188f6ba5f305a135aeceb403ede0f326
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-04-27 15:34:46 +0200

    bug fixes and fixed coin particles

--- Assets/doorTrigger.cs
+++ Assets/doorTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class doorTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
             bossWall.bossDoor = true;
