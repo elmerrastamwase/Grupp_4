b24b7f4f539a8191149ba8983a591e6c6b2b7826
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-04-27 15:28:14 +0200

    boss done

---
+++ Assets/doorTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            bossWall.bossDoor = true;
+    }
+}
