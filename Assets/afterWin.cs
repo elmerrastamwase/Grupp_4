7cfea320311509fbb1e8c0e8fee018b8fcadca61
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2019-04-30 09:54:19 +0200

    yeet

---
+++ Assets/afterWin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class afterWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameWin.winGame = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
